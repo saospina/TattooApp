9d53c3c49be924daeca3273831738a6c05cb6616
saospina <sergos0617@gmail.com>
2020-04-21 21:20:51 -0500

    APP_BASE_HREF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {APP_BASE_HREF} from '@angular/common';
 
 
 // Routes
@@ -37,7 +38,8 @@ import { SearcherComponent } from './components/searcher/searcher.component';
     HttpClientModule
   ],
   providers: [
-    TattoosService
+    TattoosService,
+    [{provide: APP_BASE_HREF, useValue: '/'}]
   ],
   bootstrap: [AppComponent]
 })
